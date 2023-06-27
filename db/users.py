b64708b08be97528536c53d1fc7cf174aa16ed51
franlops <franlopbri@gmail.com>
2023-06-26 22:59:59 -0600

    03 Completa CRUD Users

--- db/users.py
+++ db/users.py
@@ -12,6 +12,7 @@ class User:
                  last_name = '',
                  email = '',
                  image = '',
+                 role='',
                  id = None):
         self.id = id
         self.username = username
@@ -19,6 +20,7 @@ class User:
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
+        self.role = role
         self.image = image
 
     def save(self):
@@ -32,6 +34,21 @@ class User:
                 mydb.commit()
                 self.id = cursor.lastrowid
                 return self.id
+        else:
+            with mydb.cursor() as cursor:
+                sql = 'UPDATE users SET first_name = %s, last_name = %s, email = %s, image = %s '
+                sql += 'WHERE id = %s'
+                val = (self.first_name, self.last_name, self.email, self.image, self.id)
+                cursor.execute(sql, val)
+                mydb.commit()
+                return self.id
+            
+    def delete(self):
+        with mydb.cursor() as cursor:
+            sql = f"DELETE FROM users WHERE id = { self.id }"
+            cursor.execute(sql)
+            mydb.commit()
+            return self.id
 
     @staticmethod
     def __get__(id):
@@ -42,13 +59,14 @@ class User:
             user = cursor.fetchone()
 
             if user:
-                user = User(user["username"], #username
-                            user["password"], #password
-                            user["first_name"], #first_name
-                            user["last_name"], #last_name
-                            user["email"], #email
-                            user["image"], #image
-                            id)
+                user = User(username=user["username"], #username
+                            password=user["password"], #password
+                            first_name=user["first_name"], #first_name
+                            last_name=user["last_name"], #last_name
+                            email=user["email"], #email
+                            image=user["image"], #image
+                            role=user["role"],
+                            id=id)
                 return user
             
             return None
@@ -65,6 +83,25 @@ class User:
                 if check_password_hash(user["password"], password):
                     return User.__get__(user["id"])
             return None
+
+    @staticmethod
+    def get_all():
+        users = []
+        with mydb.cursor(dictionary=True) as cursor:
+            sql = f"SELECT * FROM users"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            for user in result:
+                users.append(
+                    User(username=user["username"],
+                         password=user["password"],
+                         email=user["email"],
+                         first_name=user["first_name"],
+                         last_name=user["last_name"],
+                         role=user["role"],
+                         id=user["id"])
+                )
+            return users
 
     def __str__(self):
         return f"{ self.username } { self.first_name } { self.last_name }"
